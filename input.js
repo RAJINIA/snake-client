b8af92856df0c67c8d554475f131d5fbfa681756
RAJINIA <rajini1608@gmail.com>
2021-11-04 23:23:06 -0600

    commit input after refactor

---
+++ input.js
@@ -0,0 +1,41 @@
+const connect = require("./client");
+
+
+const setupInput = function () {
+  const stdin = process.stdin;      //stdin object allow us to listen for keyboard input
+  stdin.setRawMode(true);           //configuration settings D'ONT WORRY
+  stdin.setEncoding("utf8");        //configuration settings D'ONT WORRY
+  stdin.resume();
+  return stdin;
+};
+const conn = connect();
+const handleUserInput = function (key) {
+  //console.log(key);
+  if (key === '\u0003') {                         // key for control C
+    process.exit();
+  } else if (key === 'u'){
+    setTimeout(() => {conn.write('Move: up');}, 500);
+  } else if (key === 'l'){
+    setTimeout(() => {conn.write('Move: left');}, 1000);
+  } else if (key === 'd'){
+    setTimeout(() => {conn.write('Move: down');}, 1500);
+  } else if (key === 'r'){
+    setTimeout(() => {conn.write('Move: right');}, 2000);
+  } 
+};
+
+setupInput().on('data', (key) => {
+  handleUserInput(key);
+});
+
+// return stdin;
+// };
+
+
+
+//console.log("Connecting ...");
+//connect();
+
+//setupInput(connect());
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,41 +1,34 @@
 const connect = require("./client");
+const setupInput = require('./input');
 
-const net = require("net");
-//const { stdin } = require("process");
+//const net = require("net");
 
-const obj = {
-  u: "Move: up",
-  l: "Move: Left",
-  d: "Move: Down",
-  r: "Move: Right"
-};
+// const setupInput = function () {
+//   const stdin = process.stdin;      //stdin object allow us to listen for keyboard input
+//   stdin.setRawMode(true);           //configuration settings D'ONT WORRY
+//   stdin.setEncoding("utf8");        //configuration settings D'ONT WORRY
+//   stdin.resume();
+//   return stdin;
+// };
+// const conn = connect();
+// const handleUserInput = function (key) {
+//   //console.log(key);
+//   if (key === '\u0003') {                         // key for control C
+//     process.exit();
+//   } else if (key === 'u'){
+//     setTimeout(() => {conn.write('Move: up');}, 500);
+//   } else if (key === 'l'){
+//     setTimeout(() => {conn.write('Move: left');}, 1000);
+//   } else if (key === 'd'){
+//     setTimeout(() => {conn.write('Move: down');}, 1500);
+//   } else if (key === 'r'){
+//     setTimeout(() => {conn.write('Move: right');}, 2000);
+//   } 
+// };
 
-const setupInput = function () {
-  const stdin = process.stdin;      //stdin object allow us to listen for keyboard input
-  stdin.setRawMode(true);           //configuration settings D'ONT WORRY
-  stdin.setEncoding("utf8");        //configuration settings D'ONT WORRY
-  stdin.resume();
-  return stdin;
-};
-const conn = connect();
-const handleUserInput = function (key) {
-  //console.log(key);
-  if (key === '\u0003') {                         // key for control C
-    process.exit();
-  } else if (key === 'u'){
-    setTimeout(() => {conn.write('Move: up');}, 500);
-  } else if (key === 'l'){
-    setTimeout(() => {conn.write('Move: left');}, 1000);
-  } else if (key === 'd'){
-    setTimeout(() => {conn.write('Move: down');}, 1500);
-  } else if (key === 'r'){
-    setTimeout(() => {conn.write('Move: right');}, 2000);
-  } 
-};
-
-setupInput().on('data', (key) => {
-  handleUserInput(key);
-});
+// setupInput().on('data', (key) => {
+//   handleUserInput(key);
+// });
 
 console.log("Connecting ...");
 connect();
